e3a89afe97213899f09fa80d4d45de19ef2e1379
Misha <mishakl10@ukr.net>
2021-04-09 14:51:26 +0300

    Open the Restaurant info

--- src/features/map/screens/MapScreen.js
+++ src/features/map/screens/MapScreen.js
@@ -11,7 +11,7 @@ const Map = styled(MapView)`
   width: 100%;
 `
 
-export const MapScreen = () => {
+export const MapScreen = ({ navigation }) => {
   const { location } = useContext(LocationContext)
   const { viewport, lat, lng } = location
   const { restaurants = [] } = useContext(RestaurantContext)
@@ -49,7 +49,9 @@ export const MapScreen = () => {
               longitude: restaurant.geometry.location.lng
             }}
           >
-            <MapView.Callout>
+            <MapView.Callout
+              onPress={() => navigation.navigate('RestaurantDetail', { restaurant })}
+            >
               <MapCallout restaurant={restaurant} />
             </MapView.Callout>
           </MapView.Marker>
